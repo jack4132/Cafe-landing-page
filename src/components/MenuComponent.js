01a1ea8ed8ee8692db8aeeb82140994640722613
Jackson <jacksonthounaojam1@gmail.com>
2020-11-25 13:37:02 +0530

    Single Page Application Part 1

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Card,CardImg,CardImgOverlay,CardTitle } from 'reactstrap';
-  
+ 
+/* Two ways of declaring function is shown */
+//last function contains the first function
+
 function RenderMenuItem({dish,onClick}){
   return(
     <Card  onClick={() => onClick(dish.id)}>
@@ -12,6 +15,8 @@ function RenderMenuItem({dish,onClick}){
 
   );
 }
+
+/* This is similar to declaring function as in above. Just replace const with function and use props as function argument just like the former one.*/
 
 const Menu = (props) => { 
       const menu = props.dishes.map((dish) => {
